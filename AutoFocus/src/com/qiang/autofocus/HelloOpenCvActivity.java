3126b776746e752b6b1f4dbec34d27b34de8b7ea
zhangtemplar <zhangtemplar@gmail.com>
2014-02-12 16:21:20 -0700

    We reorganize the code to have the image signature as an independent class and as a function to control the manual focus.

--- AutoFocus/src/com/qiang/autofocus/HelloOpenCvActivity.java
+++ AutoFocus/src/com/qiang/autofocus/HelloOpenCvActivity.java
@@ -19,7 +19,11 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener;
 import org.opencv.android.JavaCameraView;
 
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
@@ -27,10 +31,32 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public class HelloOpenCvActivity extends Activity implements CvCameraViewListener2{
 	private static final String  TAG = "Sample::Puzzle15::Activity";
 
     private CameraBridgeViewBase mOpenCvCameraView;
+    
+    // for control the hardware camera, e.g., focus
+    private Camera camera;
+    
+    // the saliency detector
+    private ImageSignature mImageSignature;
+    
+    // size of image
+    private int row, col;
+    
+    // size of salient object
+    private Size win_size;
+    
+    // location of salient object;
+    private Rect win;
+    
+    /**
+     * those parameters are for computing the saliency map and check the focus region
+     * we create those variables during class initialization to remove the overhead of
+     * creating them everytime we get a frame
+     */
     
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 
@@ -61,9 +87,22 @@ public class HelloOpenCvActivity extends Activity implements CvCameraViewListene
 	     super.onCreate(savedInstanceState);
 	     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	     setContentView(R.layout.helloopencvlayout);
+	     /**
+	      * initialize
+	      */
+	     row=240;
+	     col=320;
+	     win_size=new Size(32,32);
+	     
+	     mImageSignature=new ImageSignature(row, col);
+	     
+	     win=new Rect();
+	     
 	     mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
 	     mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 	     mOpenCvCameraView.setCvCameraViewListener(this);
+	     mOpenCvCameraView.setMaxFrameSize(col, row);
+	     
 	}
 
 	@Override
@@ -86,102 +125,14 @@ public class HelloOpenCvActivity extends Activity implements CvCameraViewListene
 		// add the saliency code here
 		// grab a frame
 		Mat img=inputFrame.rgba();
-		try
-		{
-			List<Mat> rgb=new ArrayList<Mat>(3);
-			Core.split(img, rgb);
-			// convert the opponent color space
-			List<Mat> opponent=rgb2opponent(rgb.get(0),rgb.get(1),rgb.get(2));
-			// apply the FFT on each channel
-			Mat sal=new Mat(img.rows(),img.cols(),CvType.CV_32F);;
-			Core.add(imageSignature(opponent.get(0)), imageSignature(opponent.get(1)), sal);
-			Core.add(imageSignature(opponent.get(2)), sal, sal);
-			// apply the smooth and find the salient window
-			// which can be achieved with a box filter
-			Rect win=findSalientWindow(sal, new Size(64,64));
-			// draw the results to the image
-			Core.rectangle(img, win.tl(), win.br(), new Scalar(128,255,0));
-		}
-		catch (NullPointerException e)
-		{
-			Log.i(TAG, "error happens during saliency computation");
-		}
+		// compute the saliency map
+		mImageSignature.computeSaliency(img);
+		mImageSignature.getSaliencyObject(win_size, win);
+		// draw the results to the image
+		Core.rectangle(img, win.tl(), win.br(), new Scalar(128,255,0));
 		return img;
 	}
-	
-	/**
-	 * this function find the salient window from the image
-	 * which is the window with maximal sum of salient score
-	 * @param sal
-	 * @return
-	 */
-	private Rect findSalientWindow(Mat sal, Size win_size)
-	{
-		Mat score=new Mat(sal.rows(),sal.cols(),sal.type());
-		Imgproc.boxFilter(sal, score, -1, win_size);
-		Core.MinMaxLocResult result=Core.minMaxLoc(score);
-		return new Rect((int)(result.maxLoc.x-win_size.width/2),
-				(int)(result.maxLoc.y-win_size.height/2),
-				(int)win_size.width, (int)win_size.height);
-	}
-	
-	/**
-	 * this function converts rgb to opponent
-	 * @param r
-	 * @param g
-	 * @param b
-	 * @return
-	 */
-	private List<Mat> rgb2opponent(Mat r, Mat g, Mat b)
-	{
-		List<Mat> channel=new ArrayList<Mat>(3);
-		Mat r2=new Mat(r.rows(),r.cols(),CvType.CV_32F);
-		r.convertTo(r2, CvType.CV_32F);
-		Mat g2=new Mat(r.rows(),r.cols(),CvType.CV_32F);
-		g.convertTo(g2, CvType.CV_32F);
-		Mat b2=new Mat(r.rows(),r.cols(),CvType.CV_32F);
-		b.convertTo(b2, CvType.CV_32F);
-		Mat y=new Mat(r.rows(),r.cols(),CvType.CV_32F);
-		Core.add(r2,g2,y);
-		Mat rg=new Mat(r.rows(),r.cols(),CvType.CV_32F);
-		Core.subtract(r2, g2, rg);
-		Mat rgb=new Mat(r.rows(),r.cols(),CvType.CV_32F);
-		Core.subtract(b2,y,rgb);
-		Core.add(r2,y,y);
-		channel.add(y);
-		channel.add(rg);
-		channel.add(rgb);
-		return channel;
-	}
-	
-	/**
-	 * this function compute the saliency map with image signature
-	 * @param img
-	 * @return
-	 */
-	private Mat imageSignature(Mat img)
-	{
-		Mat coef=new Mat(img.rows(),img.cols(),CvType.CV_32FC2);
-		// forward dft
-		Core.dft(img,coef,Core.DFT_COMPLEX_OUTPUT,img.rows());
-		// unify the magnitude
-		List<Mat> coef_complex=new ArrayList<Mat>(2);
-		Core.split(coef,coef_complex);
-		Mat mag=new Mat(img.rows(),img.cols(),CvType.CV_32F);
-		Core.magnitude(coef_complex.get(0), coef_complex.get(1), mag);
-		Core.divide(coef_complex.get(0),mag,coef_complex.get(0));
-		Core.divide(coef_complex.get(1),mag,coef_complex.get(1));
-		Core.merge(coef_complex, coef);
-		// apply the inverse dft
-		Mat sal=new Mat(img.rows(),img.cols(),CvType.CV_32FC2);
-		Core.dft(coef,sal,Core.DCT_INVERSE,img.rows());
-		// take square and magnitude
-		Core.split(sal, coef_complex);
-		Core.magnitude(coef_complex.get(0), coef_complex.get(1), mag);
-		Core.multiply(mag, mag, mag);
-		// perhaps smooth the image
-		return mag;
-	}
+
 
 	@Override
 	public void onCameraViewStarted(int width, int height) {
@@ -208,5 +159,190 @@ public class HelloOpenCvActivity extends Activity implements CvCameraViewListene
 	     if (mOpenCvCameraView != null)
 	         mOpenCvCameraView.disableView();
 	 }
+	 
+	 /**
+	  * function for manually select the focus region
+	  * @param tfocusRect
+	  */
+	 public void touchFocus(final Rect tfocusRect)
+	 {
+    	camera.stopFaceDetection();
+		
+    	//Convert from View's width and height to +/- 1000
+		final android.graphics.Rect targetFocusRect = new android.graphics.Rect(
+			tfocusRect.x * 2000/col - 1000,
+			tfocusRect.y * 2000/row - 1000,
+			(tfocusRect.x+tfocusRect.width) * 2000/row - 1000,
+			(tfocusRect.y+tfocusRect.height) * 2000/col - 1000);
+		
+		final List<Camera.Area> focusList = new ArrayList<Camera.Area>();
+		Camera.Area focusArea = new Camera.Area(targetFocusRect, 1000);
+		focusList.add(focusArea);
+		
+		Parameters para = camera.getParameters();
+		para.setFocusAreas(focusList);
+		para.setMeteringAreas(focusList);
+		camera.setParameters(para);
+    }
 }
 
+/**
+ * this class implements the image signature for computing a saliency map from a given image
+ * @author qzhang53
+ *
+ */
+class ImageSignature
+{
+    // the size of frame
+    private int row, col;
+    // the saliency map
+    private Mat sal;
+    // for finding the most salient region
+    private Mat score;
+    // the most salient window or the focus window
+    // private Rect win;
+    // the splited RGB
+    private List<Mat> rgb;
+    // the connverted opponent
+    private List<Mat> opponent;
+    // the fouerier coeffcient
+    private Mat coef;
+    private List<Mat> coef_complex;
+    private Mat mag;
+    private Mat signature;
+	
+    /**
+     * constructor which takes the size of image (r,c) as input
+     * @param r
+     * @param c
+     */
+	public ImageSignature(int r, int c)
+	{
+		row=r;
+		col=c;
+		sal=new Mat(row, col, CvType.CV_32FC1);
+		score=new Mat(row, col, CvType.CV_32FC1);
+		rgb=new ArrayList<Mat>(3);
+		opponent=new ArrayList<Mat>(6);
+		/**
+		 * the opponent is a list of mat, where the first three elements 
+		 * are the image in opponent color space, and the remaining three
+		 * are some temporary result
+		 */
+     	opponent.add(new Mat(row, col, CvType.CV_32FC1));
+     	opponent.add(new Mat(row, col, CvType.CV_32FC1));
+     	opponent.add(new Mat(row, col, CvType.CV_32FC1));
+     	opponent.add(new Mat(row, col, CvType.CV_32FC1));
+     	opponent.add(new Mat(row, col, CvType.CV_32FC1));
+     	opponent.add(new Mat(row, col, CvType.CV_32FC1));
+		coef=new Mat(row, col, CvType.CV_32FC2);
+		coef_complex=new ArrayList<Mat>(2);
+		mag=new Mat(row, col,CvType.CV_32F);
+		signature=new Mat(row, col,CvType.CV_32FC2);		
+	}
+	
+	/**
+	 * compute the saliency map for the given image
+	 * @param img
+	 */
+	public void computeSaliency(Mat img)
+	{
+		assert(img.rows()==row && img.cols()==col);
+		
+		Core.split(img, rgb);
+		// convert the opponent color space
+		rgb2opponent(rgb.get(0),rgb.get(1),rgb.get(2), opponent);
+		// apply the image signature on each channel
+		imageSignature(opponent.get(0),mag,coef,coef_complex,signature);
+		Core.add(sal,mag,sal);
+		imageSignature(opponent.get(1),mag,coef,coef_complex,signature);
+		Core.add(sal,mag,sal);
+		imageSignature(opponent.get(2),mag,coef,coef_complex,signature);
+		Core.add(sal,mag,sal);		
+	}
+	
+	/**
+	 * get the saliency map, we will (deep) copy the saliency mat to the provided parameter
+	 * @param sal
+	 */
+	public void getSaliencyMap(Mat sal)
+	{
+		this.sal.copyTo(sal);
+	}
+	
+	/**
+	 * get the most saliency window
+	 * @param img	the rgb image
+	 * @param win_size	the size of window
+	 * @return win	the window we found
+	 */
+	public void getSaliencyObject(Size win_size, Rect win)
+	{
+		// apply the smooth and find the salient window
+		// which can be achieved with a box filter
+		findSalientWindow(sal, win_size, win, score);
+	}
+	
+	/**
+	 * this function find the salient window from the image
+	 * which is the window with maximal sum of salient score
+	 * @param sal
+	 * @return
+	 */
+	private void findSalientWindow(Mat sal, Size win_size, Rect win, Mat score)
+	{
+		Imgproc.boxFilter(sal, score, -1, win_size);
+		Core.MinMaxLocResult result=Core.minMaxLoc(score);
+		win.x=(int)(result.maxLoc.x-win_size.width/2);
+		win.y=(int)(result.maxLoc.y-win_size.height/2);
+		win.width=(int)win_size.width;
+		win.height=(int)win_size.height;
+	}
+	
+	/**
+	 * this function converts rgb to opponent
+	 * @param r
+	 * @param g
+	 * @param b
+	 * @return opponent
+	 */
+	private void rgb2opponent(Mat r, Mat g, Mat b, List<Mat> opponent)
+	{	
+		// converts to float type
+		r.convertTo(opponent.get(3), CvType.CV_32F);
+		g.convertTo(opponent.get(4), CvType.CV_32F);
+		b.convertTo(opponent.get(5), CvType.CV_32F);
+		
+		// convert the color space
+		Core.add(opponent.get(3),opponent.get(4),opponent.get(0));
+		Core.subtract(opponent.get(3), opponent.get(4), opponent.get(1));
+		Core.subtract(opponent.get(5),opponent.get(0),opponent.get(2));
+		Core.add(opponent.get(3),opponent.get(0),opponent.get(0));
+	}
+	
+	/**
+	 * this function compute the saliency map with image signature
+	 * @param img
+	 * @return
+	 */
+	private void imageSignature(Mat img, Mat mag, Mat coef, List<Mat> coef_complex, Mat signature)
+	{
+		// forward dft
+		Core.dft(img,coef,Core.DFT_COMPLEX_OUTPUT,img.rows());
+		// unify the magnitude
+		
+		Core.split(coef,coef_complex);
+		
+		Core.magnitude(coef_complex.get(0), coef_complex.get(1), mag);
+		Core.divide(coef_complex.get(0),mag,coef_complex.get(0));
+		Core.divide(coef_complex.get(1),mag,coef_complex.get(1));
+		Core.merge(coef_complex, coef);
+		// apply the inverse dft
+		
+		Core.dft(coef,signature,Core.DCT_INVERSE,img.rows());
+		// take square and magnitude
+		Core.split(signature, coef_complex);
+		Core.magnitude(coef_complex.get(0), coef_complex.get(1), mag);
+		Core.multiply(mag, mag, mag);
+	}
+}
